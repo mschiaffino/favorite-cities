55650dfdc5ddf7e3b680055d91bbf0ce9aa8c705
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-11-01 22:55:09 -0300

    Patch preferred on click

--- src/components/CityListItem.tsx
+++ src/components/CityListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { ListChildComponentProps } from 'react-window';
 import ListItem from '@material-ui/core/ListItem';
@@ -7,7 +7,11 @@ import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
 import Checkbox from '@material-ui/core/Checkbox';
 
-import { cityByIndexSelector, isPreferredSelector } from '../store/citiesSlice';
+import {
+  cityByIndexSelector,
+  isPreferredSelector,
+  patchPreferred,
+} from '../store/citiesSlice';
 
 const useStyles = makeStyles({
   listItem: {
@@ -20,11 +24,24 @@ export default function CityListItem({
   style,
 }: ListChildComponentProps) {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const city = useSelector(cityByIndexSelector(index));
   const isPreferreed = useSelector(isPreferredSelector(city?.geonameid));
 
+  const onClick = () => {
+    dispatch(
+      patchPreferred({ geoNameId: city.geonameid, value: !isPreferreed })
+    );
+  };
+
   return (
-    <ListItem button style={style} key={index} className={classes.listItem}>
+    <ListItem
+      button
+      style={style}
+      key={index}
+      className={classes.listItem}
+      onClick={onClick}
+    >
       <Checkbox checked={isPreferreed} edge="start" />
       <ListItemText
         primary={city?.name || 'Loading...'}
