d76618536532e595b340f57e0480ef8a8d6b40c5
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-11-02 01:21:05 -0300

    Handle case of no favorite cities selected

--- src/components/PreferedCitiesList.tsx
+++ src/components/PreferedCitiesList.tsx
@@ -3,8 +3,13 @@ import { useSelector } from 'react-redux';
 
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
+import Box from '@material-ui/core/Box/Box';
+import Typography from '@material-ui/core/Typography';
 
-import { preferredCitiesIdsSelector } from '../store/citiesSlice';
+import {
+  loadingPreferredSelector,
+  preferredCitiesIdsSelector,
+} from '../store/citiesSlice';
 import PreferedCitiesListItem from './PreferedCitiesListItem';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -19,9 +24,15 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function PreferedCitiesList() {
   const classes = useStyles();
   const preferredCitiesIds = useSelector(preferredCitiesIdsSelector);
+  const loadingPreferred = useSelector(loadingPreferredSelector);
 
   return (
     <List component="nav" className={classes.root} aria-label="contacts">
+      {!loadingPreferred && !preferredCitiesIds.length && (
+        <Box paddingLeft={1}>
+          <Typography>No cities selected yet</Typography>
+        </Box>
+      )}
       {preferredCitiesIds.map((id) => (
         <PreferedCitiesListItem geoNameId={id} />
       ))}
