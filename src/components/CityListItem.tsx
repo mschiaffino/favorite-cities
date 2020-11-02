7850ec13554e9a112ed967e421b031a61f8c8201
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-11-01 22:58:34 -0300

    Show spinner while saving preference changes

--- src/components/CityListItem.tsx
+++ src/components/CityListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { ListChildComponentProps } from 'react-window';
@@ -6,6 +6,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
 import Checkbox from '@material-ui/core/Checkbox';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import {
   cityByIndexSelector,
@@ -27,11 +28,17 @@ export default function CityListItem({
   const dispatch = useDispatch();
   const city = useSelector(cityByIndexSelector(index));
   const isPreferreed = useSelector(isPreferredSelector(city?.geonameid));
+  const [savingChange, setSavingChange] = useState(false);
 
-  const onClick = () => {
-    dispatch(
+  const onClick = async () => {
+    if (savingChange) {
+      return;
+    }
+    setSavingChange(true);
+    await dispatch(
       patchPreferred({ geoNameId: city.geonameid, value: !isPreferreed })
     );
+    setSavingChange(false);
   };
 
   return (
@@ -47,6 +54,7 @@ export default function CityListItem({
         primary={city?.name || 'Loading...'}
         secondary={city ? `${city.subcountry} - ${city.country} ` : undefined}
       />
+      {savingChange && <CircularProgress />}
     </ListItem>
   );
 }
